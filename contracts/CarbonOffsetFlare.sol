bbe5ce6ac8b99992c5098cdc0bdd267f8d44ce3d
Giulio de Cadilhac <g.decadilhac@gmail.com>
2025-05-31 21:32:34 +0200

    update flare contract

--- contracts/CarbonOffsetFlare.sol
+++ contracts/CarbonOffsetFlare.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.0;
 
 import "@flarenetwork/flare-periphery-contracts/coston2/IWeb2Json.sol";
+import {ContractRegistry} from "@flarenetwork/flare-periphery-contracts/coston2/ContractRegistry.sol";
 
 contract CarbonOffset {
     address public stargateBridge;
@@ -24,16 +25,13 @@ contract CarbonOffset {
         address recipientAddress,
         uint256 maxAmountReceived
     ) public {
-        // Calculate the amount of USDC needed
         uint256 amountUSDCneeded = calculateUSDCNeeded(proof, rate);
 
-        // Ensure the amount is within the max allowed
         require(
             amountUSDCneeded <= maxAmountReceived,
             "Amount exceeds max allowed"
         );
 
-        // Interact with the Stargate bridge to send USDC
         bridgeUSDC(amountUSDCneeded, recipientAddress);
     }
 
@@ -41,11 +39,28 @@ contract CarbonOffset {
         bytes memory proof,
         uint256 rate
     ) internal pure returns (uint256) {
-        // Implement your logic to calculate the USDC needed
         return 0.01 * 10 ** 6;
     }
 
     function bridgeUSDC(uint256 amount, address recipient) internal {
         // Implement the logic to interact with the Stargate bridge
+    }
+
+    function addCarbonOffset(IWeb2Json.Proof calldata proof) public {
+        require(isJsonApiProofValid(proof), "Invalid proof");
+
+        CarbonOffsetData memory data = abi.decode(
+            proof.data.responseBody.abiEncodedData,
+            (CarbonOffsetData)
+        );
+
+        // Use data.recipientAddress, data.recipientGas, and data.rate
+        // Implement your business logic here
+    }
+
+    function isJsonApiProofValid(
+        IWeb2Json.Proof calldata _proof
+    ) private view returns (bool) {
+        return ContractRegistry.getFdcVerification().verifyJsonApi(_proof);
     }
 }
